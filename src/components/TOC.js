7ef24cca09e49586bd6411b0daa60ca5c208eaf2
qkr0677 <qkr0677@gmail.com>
2021-01-13 14:00:29 +0900

    add states

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 
 class TOC extends Component{
-  render(){
-    return (
-      <nav>
-        <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-   );
-  }
+    render(){
+        var lists = [];
+        var date = this.props.data;
+        var i = 0;
+        while(i<data.length){
+            lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+            i=i+1;
+        }
+        return (
+            <nav>
+                <ul>
+                    <li><a href="1.html">HTML</a></li>
+                    <li><a href="2.html">CSS</a></li>
+                    <li><a href="3.html">JavaScript</a></li>
+                </ul>
+            </nav>
+        );
+    }
 }
 
 export default TOC;
