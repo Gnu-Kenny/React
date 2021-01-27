c7506bb8845f7ba8a0a5dcfc2410ba39eaa6ab27
qkr0677 <qkr0677@gmail.com>
2021-01-28 01:25:29 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import TOC from "./components/TOC";
 import Subject from "./components/Subject";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
+import Control from "./components/Control";
 import './App.css';
+
 
 
 
@@ -10,8 +14,10 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props);
+    // id++
+    this.max_content_id = 3;
     this.state = {
-      mode:'read',
+      mode:'create',
       selected_content_id:2,
       subject:{title:'WEB', sub:"world wide Web!"},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
@@ -22,29 +28,73 @@ class App extends Component {
       ]
     }
   }
-  render() {
+  getReadContent(){
+    var i = 0;
+    while( i< this.state.contents.length){
+      var data = this.state.contents[i];
+      if(data.id === this.state.selected_content_id){
+        return data;
+        break;
+      }
+     i= i +1;
+    }
+  }
+  getContent(){
     console.log('App render');
     
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome')
     {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc; //본문
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
 
     }
     else if(this.state.mode === 'read'){
-      var i = 0;
-      while( i< this.state.contents.length){
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id){
-          _title = data.title;
-          _desc = data.desc; 
-          break;
-        }
-       i= i +1;
-      }
-      
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+    } else if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={function(_title, _desc){
+        //add content to this.state.contents
+        // this.max_content_id = this.max_content_id+1;
+        // this.state.contents.push(
+        //   {id:this.max_content_id, title:_title, desc:_desc}
+        // );
+        // 위의 방법도 가능하나 original 변경시킴
+
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        )
+        
+        this.setState({
+          contents:_contents
+        });
+        console.log(_title,_desc);
+      }.bind(this)}></CreateContent>
+    } else if(this.state.mode === 'update'){
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={function(_title, _desc){
+        //add content to this.state.contents
+        // this.max_content_id = this.max_content_id+1;
+        // this.state.contents.push(
+        //   {id:this.max_content_id, title:_title, desc:_desc}
+        // );
+        // 위의 방법도 가능하나 original 변경시킴
+
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        )
+        
+        this.setState({
+          contents:_contents
+        });
+        console.log(_title,_desc);
+      }.bind(this)}></UpdateContent>
     }
+    return _article;
+  }
+  render() {
+    
   return ( 
       <div className="App">
         <Subject //이 컴포넌트는 onChangePage라는 이벤트가 있어서 이 컴포넌트 안에서 링크를 클릭했을때 이 이벤트에 설치한 함수를 호출하도록 만듬
@@ -65,7 +115,12 @@ class App extends Component {
         }.bind(this)}
         data={this.state.contents}
         ></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          })
+        }.bind(this)}></Control>
+        {this.getContent()}
       </div>
     );
   }
